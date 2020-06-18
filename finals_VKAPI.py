9e79b502645b0c847fa8919697980a3d6e73e2c9
evgeniyism <58936612+evgeniyism@users.noreply.github.com>
2020-06-18 20:52:56 +0300

    Add files via upload

---
+++ finals_VKAPI.py
@@ -0,0 +1,9 @@
+import finals_logic
+from FL_constants import USER_ID
+from FL_constants import API_TOKEN
+
+if __name__ == '__main__':
+    user_id = USER_ID
+    api_token = API_TOKEN
+    finals_logic.start(user_id, api_token)
+
